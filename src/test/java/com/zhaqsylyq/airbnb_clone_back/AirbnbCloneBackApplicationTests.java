6b559c45342a6ea8cdccd97b443310d4b8cba27c
zhaqsylyq <zhaksylyk.chalgimbayev@nu.edu.kz>
2024-08-02 23:28:56 +0500

    first commit

---
+++ src/test/java/com/zhaqsylyq/airbnb_clone_back/AirbnbCloneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaqsylyq.airbnb_clone_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbCloneBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
